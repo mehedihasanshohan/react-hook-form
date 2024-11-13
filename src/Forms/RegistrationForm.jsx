f977903435de85a301689c4037df88f095673619
mehedi hasan <mhshohan04@gmail.com>
2024-11-13 16:24:51 +0600

    dynamic input handle

--- src/Forms/RegistrationForm.jsx
+++ src/Forms/RegistrationForm.jsx
@@ -3,7 +3,7 @@
 
 import Field from "../components/Field"
 import FieldSet from "../components/FieldSet"
-import {useForm} from 'react-hook-form';
+import {useForm, useFieldArray} from 'react-hook-form';
 
 
 const RegistrationForm = () => {
@@ -12,7 +12,13 @@ const RegistrationForm = () => {
     handleSubmit,
     formState: { errors },
     setError,
+    control
   } = useForm();
+
+  const {fields, append, remove} = useFieldArray({
+    name: "socials",
+    control,
+  });
 
   const submitForm = (formData) => {
     console.log(formData);
@@ -83,6 +89,45 @@ const RegistrationForm = () => {
             </Field>
 
           </FieldSet>
+
+          <FieldSet label="Enter Socials Handles">
+            {
+              fields.map((field, index) => {
+                return (
+                  <div
+                      className="flex justify-between items-center w-max"
+                      key={field.id}>
+                           <Field label="Social Name">
+                            <input
+                                className="p-2 border box-border w-full rounded-md"
+                                {...register(`socials[${index}].name`)}
+                                type="text"
+                                name={`socials[${index}].name`}
+                                id={`socials[${index}].name`}
+                                placeholder="Enter Your Social Platform">
+                            </input>
+                         </Field>
+                           <Field label="Social URL">
+                            <input
+                                className="p-2 border box-border w-full rounded-md"
+                                {...register(`socials[${index}].url`)}
+                                type="text"
+                                name={`socials[${index}].url`}
+                                id={`socials[${index}].url`}
+                                placeholder="Enter Your Social URL">
+                            </input>
+                         </Field>
+                  </div>
+                )
+              })
+            }
+              <button
+                className="mt-8 text-md text-white cursor-pointer
+                  border rounded-lg bg-gray-500 p-1 m-auto"
+                onClick={() => append({name: "", url: ""})}
+                >Add a Social Handle</button>
+          </FieldSet>
+
           <div>{errors?.root?.random?.message}</div>
           <Field>
                 <button
